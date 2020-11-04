f9e69b5c55ec09ddf215bb3fe76ca2831ce8a7d7
Shawn P. Serbin <sserbin@bnl.gov>
2020-11-04 15:08:07 -0500

    Added initial example script for pulling down met forcing data from OSF

---
+++ scripts/download_singlesite_forcing_data.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# =======================================================================================
+#
+# INSTALL Python OSF API Client
+# pip3 install osfclient=
+#
+#
+# Usage guide
+# https://osfclient.readthedocs.io/en/latest/cli-usage.html
+# =======================================================================================
+
+# =======================================================================================
+# OPTIONS
+cesm_data_dir=~/Data/cesm_input_datasets/single_site
+mkdir -p ${cesm_data_dir}
+# =======================================================================================
+
+# =======================================================================================
+echo "*** Downloading and extracting forcing data from OSF ***"
+
+### get remote forcing data from OSF
+# osf fetch --help
+osf -p kv93n fetch -U single_site_forcing/GSWP3_PA-SLZ_met_forcing_partial.tar.gz \
+${cesm_data_dir}/GSWP3_PA-SLZ_met_forcing_partial.tar.gz
+
+### extract forcing data
+cd ${cesm_data_dir}
+tar -zxf GSWP3_PA-SLZ_met_forcing_partial.tar.gz
+# =======================================================================================
+
+# =======================================================================================
+# done
+echo "*** DONE ***"
+# =======================================================================================
